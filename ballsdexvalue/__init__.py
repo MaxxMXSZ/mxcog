89c9fb95c93ea4195a2bc069f3f92e6c30bcbdef
LeMx <153226500+MaxxMXSZ@users.noreply.github.com>
2024-01-14 18:45:52 +0100

    Update __init__.py

--- ballsdexvalue/__init__.py
+++ ballsdexvalue/__init__.py
@@ -3,5 +3,5 @@ from redbot.core import bot
 from .ballsdexvalue import BallsdexValue
 
 async def setup(bot: Red) -> None:
-    cog = CodeSnippets(bot)
+    cog = BallsdexValue(bot)
     await bot.add_cog(cog)
